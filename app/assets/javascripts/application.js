a611b25c3cd6a6f7e1239ae1ccafcd7b45fd6181
jetamartin <jetamartin@yahoo.com>
2016-12-07 05:00:48 +0000

    Add stock lookup and ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,16 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+var hide_spinner = function(){
+  $('#spinner').hide();
+}
+var show_spinner = function() {
+  $('#spinner').show();
+}
---
+++ app/models/stock.rb
@@ -0,0 +1,31 @@
+class Stock < ActiveRecord::Base
+  # Class method to look up stock from Stock DB and return Stock object
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+  
+  # Class method to look up stock info from external source (GEM stock_quote)
+  def self.new_from_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    # if no stock found with given symbol return nil
+    return nil unless looked_up_stock.name
+    # If stock found then create and build stock object to be returned
+    new_stock = new(ticker: looked_up_stock.symbol, name: looked_up_stock.name)
+    new_stock.last_price = new_stock.price
+    new_stock
+  end
+  
+  # Price returned depends on whether market still open or already closed
+  def price
+    # if markets aren't closed there will not be a closing price yet.
+    closing_price = StockQuote::Stock.quote(ticker).close
+    return "#{closing_price} (Closing)" if closing_price
+    
+    # Opening price should be available but if not just return 'Unavailable'
+    opening_price = StockQuote::Stock.quote(ticker).open
+    return "#{opening_price} (Opening)" if opening_price
+    'Unavailable'
+  end
+  
+  
+end
