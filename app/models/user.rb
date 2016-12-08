8eaca26383cdd469e53bb1e382649ca046bb1221
jetamartin <jetamartin@yahoo.com>
2016-12-08 19:37:40 +0000

    update views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ActiveRecord::Base
   # use user_stocks.count rather than user.user_stocks.count..they are equivalent
   # statements
   
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+  
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
