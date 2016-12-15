f7797b20e18b2b43f6225123bfe4441403035a6e
jetamartin <jetamartin@yahoo.com>
2016-12-15 21:54:48 +0000

    Added a few more descriptive code comments

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,10 +1,14 @@
 class StocksController < ApplicationController
   def search
+    # debugger
     if params[:stock]
       @stock = Stock.find_by_ticker(params[:stock]) 
       @stock ||= Stock.new_from_lookup(params[:stock]) 
     end
+
     if @stock
+      # By rendering json and entering app path/search_stocks?stock=GOOG
+      # you will see jason object returned and confirm code is working
       # render json: @stock
       render partial:  'lookup'
     else
